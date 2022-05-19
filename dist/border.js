dee233ffa9183796dcb1ddda0bf730966f730f1a
Jakub Dolejs <jakubdolejs@gmail.com>
2022-05-19 22:52:20 +0100

    Simpler command to extend image edges by mirroring

--- dist/border.js
+++ dist/border.js
@@ -8,7 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 import { Colour } from "./colour.js";
-import { runCommand, pipeCommands } from "./command.js";
+import { runCommand } from "./command.js";
 export function addBorderPercent(image, x, y, colour = Colour.WHITE, outputFormat = "png") {
     return __awaiter(this, void 0, void 0, function* () {
         if (outputFormat.toLowerCase() != "png" && outputFormat.toLowerCase() != "jpg") {
@@ -22,18 +22,24 @@ export function extendSidesByMirroring(image, x, y, outputFormat = "png") {
         if (outputFormat.toLowerCase() != "png" && outputFormat.toLowerCase() != "jpg") {
             return Promise.reject(new Error("Invalid output format. Valid formats are png and jpg."));
         }
-        const cropX = 50 + x;
-        const cropY = 50 + y;
-        const commands = [
-            {
-                "command": "convert",
-                "args": ["-", "-set", "option:distort:viewport", "%[fx:w*2]x%[fx:h*2]", "-virtual-pixel", "mirror", "-distort", "srt", "0,0 1,1 0 %[fx:w/2],%[fx:h/2]", `${outputFormat}:-`]
-            }, {
-                "command": "convert",
-                "args": ["-", "-gravity", "Center", "-crop", `%${cropX}x${cropY}+0+0`, `${outputFormat}:-`]
-            }
-        ];
-        return pipeCommands(commands, image);
+        const widthMultiplier = x * 0.02 + 1;
+        const heightMultiplier = y * 0.02 + 1;
+        const xMultiplier = x * 0.01;
+        const yMultiplier = y * 0.01;
+        return runCommand({ "command": "convert", "args": ["-", "-set", "option:distort:viewport", `%[fx:w*${widthMultiplier}]x%[fx:h*${heightMultiplier}]-%[fx:w*${xMultiplier}]-%[fx:h*${yMultiplier}]`, "-virtual-pixel", "Mirror", "-filter", "point", "-distort", "SRT", "0", "+repage", `${outputFormat}:-`] }, image);
+        // const cropX = 50+x
+        // const cropY = 50+y
+        // const commands: Command[] = [
+        //     {
+        //         "command": "convert",
+        //         "args": ["-", "-set", "option:distort:viewport", `%[fx:w+${x}]x%[fx:h+${y}]-${Math.round(x/2)}-${Math.round(y/2)}`, "-virtual-pixel", "Mirror", "-filter", "point", "-distort", "SRT", "0", "+repage", `${outputFormat}:-`]
+        //         "args": ["-", "-set", "option:distort:viewport", "%[fx:w*2]x%[fx:h*2]", "-virtual-pixel", "mirror", "-distort", "srt", "0,0 1,1 0 %[fx:w/2],%[fx:h/2]", `${outputFormat}:-`]
+        //     },{
+        //         "command": "convert",
+        //         "args": ["-", "-gravity", "Center", "-crop", `%${cropX}x${cropY}+0+0`, `${outputFormat}:-`]
+        //     }
+        // ];
+        // return pipeCommands(commands, image)
     });
 }
 //# sourceMappingURL=border.js.map
--- src/border.ts
+++ src/border.ts
@@ -1,6 +1,6 @@
 import { Colour } from "./colour.js";
-import { OutputFormat, Command } from "./types.js";
-import { runCommand, pipeCommands } from "./command.js";
+import { OutputFormat } from "./types.js";
+import { runCommand } from "./command.js";
 
 export async function addBorderPercent(image: Buffer, x: number, y: number, colour: Colour = Colour.WHITE, outputFormat: OutputFormat = "png"): Promise<Buffer> {
     if (outputFormat.toLowerCase() != "png" && outputFormat.toLowerCase() != "jpg") {
@@ -13,16 +13,9 @@ export async function extendSidesByMirroring(image: Buffer, x: number, y: number
     if (outputFormat.toLowerCase() != "png" && outputFormat.toLowerCase() != "jpg") {
         return Promise.reject(new Error("Invalid output format. Valid formats are png and jpg."));
     }
-    const cropX = 50+x
-    const cropY = 50+y
-    const commands: Command[] = [
-        {
-            "command": "convert",
-            "args": ["-", "-set", "option:distort:viewport", "%[fx:w*2]x%[fx:h*2]", "-virtual-pixel", "mirror", "-distort", "srt", "0,0 1,1 0 %[fx:w/2],%[fx:h/2]", `${outputFormat}:-`]
-        },{
-            "command": "convert",
-            "args": ["-", "-gravity", "Center", "-crop", `%${cropX}x${cropY}+0+0`, `${outputFormat}:-`]
-        }
-    ];
-    return pipeCommands(commands, image)
+    const widthMultiplier = x*0.02+1
+    const heightMultiplier = y*0.02+1
+    const xMultiplier = x*0.01
+    const yMultiplier = y*0.01
+    return runCommand({"command": "convert", "args": ["-", "-set", "option:distort:viewport", `%[fx:w*${widthMultiplier}]x%[fx:h*${heightMultiplier}]-%[fx:w*${xMultiplier}]-%[fx:h*${yMultiplier}]`, "-virtual-pixel", "Mirror", "-filter", "point", "-distort", "SRT", "0", "+repage", `${outputFormat}:-`]}, image)
 }
